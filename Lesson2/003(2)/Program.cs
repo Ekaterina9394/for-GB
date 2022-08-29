1fbf74e78af6a27256720919198341a62c63ccb3
catya <catherine.mytsu@yandex.ru>
2022-08-29 11:22:44 +0300

    hw2

---
+++ Lesson2/003(2)/Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает 2 числа и выводит, 
+//является ли второе число кратным 
+//первому. Если число 2 не кратно 
+//числу 1, то программа выводит 
+//остаток от деления
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int c = a % b;
+if (c == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток = {c}");
+}
---
+++ Lesson2/005(2)/Program.cs
@@ -0,0 +1,16 @@
+﻿//принимает на вход 2 числа и проверяет является ли
+//одно число квадратом другого пример 5 25 -> да
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
