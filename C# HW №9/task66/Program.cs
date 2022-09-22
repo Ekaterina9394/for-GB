8d1e32eff40e271860ebb0ac1d00b1a1dc446980
catya <catherine.mytsu@yandex.ru>
2022-09-22 15:27:00 +0300

    C# HW №9

---
+++ C# HW №9/task66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int NaturalElementsSumm(int m, int n)
+{
+    int start = m;
+    int end = n;
+    if (m > n)
+    {
+        start = n;
+        end = m;
+    }
+    int res = (end + start) * (end - start + 1) / 2;
+    return res;
+}
+
+Console.WriteLine("Введите значение m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение n: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(NaturalElementsSumm(m, n));
